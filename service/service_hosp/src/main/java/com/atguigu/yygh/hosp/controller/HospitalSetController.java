446661cd56fd5f30cf78fde39d8e878f5123b082
BobJiang <374123140@qq.com>
2022-01-12 17:06:36 +0800

    Conditional and Pageable data retrieve

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
@@ -3,9 +3,13 @@ package com.atguigu.yygh.hosp.controller;
 import com.atguigu.yygh.common.result.Result;
 import com.atguigu.yygh.hosp.service.HospitalSetService;
 import com.atguigu.yygh.model.hosp.HospitalSet;
+import com.atguigu.yygh.vo.hosp.HospitalSetQueryVo;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
@@ -38,4 +42,35 @@ public class HospitalSetController {
             return Result.fail();
         }
     }
+
+    //3 条件查询带分页
+    @ApiOperation("条件分页查询")
+    @PostMapping("findPageHospSet/{current}/{limit}")
+    public Result findPageHospSet(@PathVariable long current,
+                                  @PathVariable long limit,
+                                  @RequestBody(required = false) HospitalSetQueryVo hospitalSetQueryVo) {
+
+        //创建page对象
+        Page<HospitalSet> hospitalSetPage = new Page<>(current, limit);
+
+        //构建查询条件
+        QueryWrapper<HospitalSet> queryWrapper = new QueryWrapper<>();
+
+        String hosname = hospitalSetQueryVo.getHosname();
+        String hoscode = hospitalSetQueryVo.getHoscode();
+
+        if(!StringUtils.isEmpty(hosname)){
+            queryWrapper.like("hosname",hosname);
+        }
+
+        if(!StringUtils.isEmpty(hoscode)){
+            queryWrapper.eq("hoscode",hoscode);
+        }
+
+        //调用分页方法
+        Page<HospitalSet> page = hospitalSetService.page(hospitalSetPage,queryWrapper);
+
+        return Result.ok(page);
+    }
+
 }
