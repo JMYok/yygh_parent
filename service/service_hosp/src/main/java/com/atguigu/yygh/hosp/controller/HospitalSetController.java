991efce2fa947bbd7e922792c9bcb20a77ea0fb3
BobJiang <374123140@qq.com>
2022-01-12 16:30:44 +0800

    Uniforming Response format:Result Entity

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
@@ -1,17 +1,17 @@
 package com.atguigu.yygh.hosp.controller;
 
+import com.atguigu.yygh.common.result.Result;
 import com.atguigu.yygh.hosp.service.HospitalSetService;
 import com.atguigu.yygh.model.hosp.HospitalSet;
-import com.mysql.cj.xdevapi.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
 import java.util.List;
 
+@Api(tags = "医院设置管理")
 @RestController
 @RequestMapping("/admin/hosp/hospitalSet")
 public class HospitalSetController {
@@ -19,15 +19,23 @@ public class HospitalSetController {
     @Autowired
     private HospitalSetService hospitalSetService;
 
+    //1 查询医院设置表所有信息
+    @ApiOperation(value = "获取所有医院设置")
     @GetMapping("/findAll")
-    public List<HospitalSet> findAllHospitalSet(){
+    public Result findAllHospitalSet(){
         List<HospitalSet> list = hospitalSetService.list();
-        return list;
+        return Result.ok(list);
     }
 
-    @DeleteMapping("/delete/{id}")
-    public boolean deleteHospitalSet(@PathParam("id") Integer id){
-        boolean is_delete = hospitalSetService.removeById(2L);
-        return is_delete;
+    //2 逻辑删除医院设置
+    @ApiOperation(value = "逻辑删除医院设置")
+    @DeleteMapping("/{id}")
+    public Result deleteHospitalSet(@PathVariable("id") Integer id){
+        boolean status = hospitalSetService.removeById(2L);
+        if(status){
+            return Result.ok();
+        }else {
+            return Result.fail();
+        }
     }
 }
