c853e0c6ab092b79bc959df6670c528981cfa789
BobJiang <374123140@qq.com>
2022-01-19 20:26:01 +0800

    Initializing data dictionary function

---
+++ service/service_cmn/src/main/java/com/atguigu/yygh/cmn/service/Impl/DictServiceImpl.java
@@ -0,0 +1,14 @@
+package com.atguigu.yygh.cmn.service.Impl;
+
+import com.atguigu.yygh.cmn.mapper.DictMapper;
+import com.atguigu.yygh.cmn.service.DictService;
+import com.atguigu.yygh.model.cmn.Dict;
+import com.atguigu.yygh.model.hosp.HospitalSet;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DictServiceImpl extends ServiceImpl<DictMapper, Dict> implements DictService {
+
+}
