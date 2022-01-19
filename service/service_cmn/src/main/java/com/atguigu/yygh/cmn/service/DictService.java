cece57b3050c04806a5363a270080894936bbd93
BobJiang <374123140@qq.com>
2022-01-19 21:05:25 +0800

    Finished findChildDataById function

--- service/service_cmn/src/main/java/com/atguigu/yygh/cmn/service/DictService.java
+++ service/service_cmn/src/main/java/com/atguigu/yygh/cmn/service/DictService.java
@@ -4,5 +4,9 @@ import com.atguigu.yygh.model.cmn.Dict;
 import com.atguigu.yygh.model.hosp.HospitalSet;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 public interface DictService extends IService<Dict> {
+    //根据数据id查询子数据列表
+    List<Dict> findChildDataById(Long id);
 }
--- service/service_cmn/src/main/java/com/atguigu/yygh/cmn/service/Impl/DictServiceImpl.java
+++ service/service_cmn/src/main/java/com/atguigu/yygh/cmn/service/Impl/DictServiceImpl.java
@@ -4,11 +4,34 @@ import com.atguigu.yygh.cmn.mapper.DictMapper;
 import com.atguigu.yygh.cmn.service.DictService;
 import com.atguigu.yygh.model.cmn.Dict;
 import com.atguigu.yygh.model.hosp.HospitalSet;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DictServiceImpl extends ServiceImpl<DictMapper, Dict> implements DictService {
 
+    //根据数据id查询子数据列表
+    @Override
+    public List<Dict> findChildDataById(Long id) {
+        QueryWrapper<Dict> wrapper = new QueryWrapper<>();
+        wrapper.eq("parent_id",id);
+        List<Dict> dicts = baseMapper.selectList(wrapper);
+        for(Dict dict : dicts){
+            boolean is_children = isChildren(dict.getId());
+            dict.setHasChildren(is_children);
+        }
+        return dicts;
+    }
+
+    //判断id下面是否有子节点
+    private boolean isChildren(Long id){
+        QueryWrapper<Dict> wrapper = new QueryWrapper<>();
+        wrapper.eq("parent_id",id);
+        Integer cnt = baseMapper.selectCount(wrapper);
+        return cnt>0;
+    }
 }
