a89e127be3c4dc0b49f9f8349317a58dacd920e5
BobJiang <374123140@qq.com>
2022-01-21 21:38:35 +0800

    Added Spring-cache: redis

--- service/service_cmn/src/main/java/com/atguigu/yygh/cmn/service/Impl/DictServiceImpl.java
+++ service/service_cmn/src/main/java/com/atguigu/yygh/cmn/service/Impl/DictServiceImpl.java
@@ -12,6 +12,8 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.apache.http.HttpResponse;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -30,6 +32,7 @@ public class DictServiceImpl extends ServiceImpl<DictMapper, Dict> implements Di
      * @return
      */
     @Override
+    @Cacheable(value = "dict",keyGenerator = "keyGenerator")
     public List<Dict> findChildDataById(Long id) {
         QueryWrapper<Dict> wrapper = new QueryWrapper<>();
         wrapper.eq("parent_id",id);
@@ -58,6 +61,7 @@ public class DictServiceImpl extends ServiceImpl<DictMapper, Dict> implements Di
      * @param file
      */
     @Override
+    @CacheEvict(value = "dict", allEntries=true)
     public void importDictData(MultipartFile file) {
         try {
             EasyExcel.read(file.getInputStream(),DictEeVo.class,new DictListener(baseMapper))
